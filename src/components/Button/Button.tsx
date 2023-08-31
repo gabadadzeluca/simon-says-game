02ac4a883e25fe23436376c2b4bfc322c42a0df7
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-01 00:14:21 +0400

    add onClick to the button; format code

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,29 @@
-export const Button = (props:{
+import styled from "styled-components";
+
+export const Button = (props: {
   color: string;
-}) =>{
-  const {color} = props;
+  highlighted: boolean;
+  onClick: ()=>void;
+}) => {
+  const { color, highlighted, onClick } = props;
   return (
-    <>{color}</>
-  )
-}
+    <SButton
+      color={color}
+      highlighted={highlighted}
+      onClick={onClick}
+    ></SButton>
+  );
+};
+
+const SButton = styled.button<{ color: string; highlighted: boolean }>`
+  all: unset;
+  background-color: ${({ color }) => color};
+  width: 20rem;
+  height: 20rem;
+  border-radius: 1rem;
+  cursor: pointer;
+  opacity: ${({ highlighted }) => (highlighted ? "1" : "0.3")};
+  &:hover {
+    opacity: 1;
+  }
+`;
