252689cef92bb8d2bad2c8ef796110041fba12cc
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-31 18:22:22 +0400

    add styles for gameBoard

--- src/components/GameBoard/GameBoard.styled.ts
+++ src/components/GameBoard/GameBoard.styled.ts
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 export const SGameBoardDiv = styled.div`
   display: grid;
+  align-items: center;
+  padding: 10rem;
+  background-color: black;
   grid-template-columns: repeat(3, 1fr);
   row-gap: 2rem;
+  column-gap: 2rem;
+  overflow: hidden;
+  justify-content: center;
 `;
