3f6b68319cffefb033aca0d9b1415da61ed23984
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-02 16:24:49 +0400

    add Level Paragraph styles

--- src/components/GameBoard/GameBoard.styled.ts
+++ src/components/GameBoard/GameBoard.styled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const SGameBoardDiv = styled.div`
   display: grid;
+  border-radius: 1.5rem;
   align-items: center;
   padding: 10rem;
   background-color: black;
@@ -11,3 +12,13 @@ export const SGameBoardDiv = styled.div`
   overflow: hidden;
   justify-content: center;
 `;
+export const SLevelP = styled.p`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 100%;
+  color: #ffffff;
+  font-size: 4rem;
+  font-weight: 600;
+  background-color: red;
+  text-align: center;
+`
